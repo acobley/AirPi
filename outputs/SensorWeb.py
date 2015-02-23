86c5e4e5552e3fac0fa3c46380074902d63ba577
Andy Cobley <acobley@computing.dundee.ac.uk>
2015-02-23 11:34:38 +0000

    json encoding

--- outputs/SensorWeb.py
+++ outputs/SensorWeb.py
@@ -7,8 +7,14 @@ class Print(output.Output):
 	def __init__(self,data):
 		pass
 	def outputData(self,dataPoints):
+	arr = []
+		for i in dataPoints:
+			arr.append({"id":i["name"],"current_value":i["value"]})
+		a = json.dumps({"version":"1.0.0","datastreams":arr})
+	
 		print ""
 		print "Time: " + str(datetime.datetime.now())
 		for i in dataPoints:
 			print "Sensor "+i["name"] + ": " + str(i["value"]) + " " + i["symbol"]
+		print "Json "+a
 		return True
