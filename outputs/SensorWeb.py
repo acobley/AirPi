d114d3ae55db0e51cdccc2efb0911fc91e36e752
Andy Cobley <acobley@computing.dundee.ac.uk>
2015-02-23 12:01:59 +0000

    Devoce attrib

--- outputs/SensorWeb.py
+++ outputs/SensorWeb.py
@@ -6,9 +6,13 @@ class Print(output.Output):
 	requiredData = []
 	optionalData = []
 	def __init__(self,data):
+		self.Device=data["Device"]
+		
+	def __init__(self,data):
 		pass
 	def outputData(self,dataPoints):
 		arr = []
+		arr.append({"Device":Device})
 		for i in dataPoints:
 			arr.append({"id":i["name"],"current_value":i["value"]})
 		a = json.dumps({"version":"1.0.0","datastreams":arr})
